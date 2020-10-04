37d7af7ca04f25027d1de70d4f3c55204d25a541
kureharyosuke <ohhayo.com@gmail.com>
2020-10-05 05:02:01 +0900

    #10. Redux, Combine reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import thunk from "redux-thunk";
+
+import Reducer from "./state";
+
 import Routes from "./Router";
 
 import "./App.css";
 
+const store = createStore(
+  Reducer,
+  composeWithDevTools(
+    applyMiddleware(
+      thunk
+      // socketMiddleware
+    )
+  )
+);
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
